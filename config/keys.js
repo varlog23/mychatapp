579b8863e129f575d59b161b9f39fdda812fca31
Varun Nayak <uvarun10@gmail.com>
2019-03-08 13:53:47 +0100

    Added Aylien key and App ID

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,9 @@
 dbPassword = 'mongodb+srv://myuchatadmin:myuchatadmin@cluster0-ouxuz.mongodb.net/test?retryWrites=true';
-
+application_id = "55e4d5dd";
+application_key = "f62afb04f36293868458419356324b6d";
 
 module.exports = {
-    mongoURI: dbPassword
+    mongoURI: dbPassword,
+    aylienAppId : application_id,
+    aylienAppKey : application_key
 };
